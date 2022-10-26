1df78714a97ec8fdee2d2ff333640ce76af35ff6
harry <zh_harry@163.com>
2022-10-27 00:05:57 +0800

    init

--- file-gateway/src/main/java/com/sparrow/file/controller/FileController.java
+++ file-gateway/src/main/java/com/sparrow/file/controller/FileController.java
@@ -1,17 +1,18 @@
 package com.sparrow.file.controller;
 
 import com.sparrow.file.api.AttachService;
-import com.sparrow.mvc.RequestParameters;
 import com.sparrow.protocol.BusinessException;
 import com.sparrow.protocol.Controller;
 import javax.inject.Inject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FileController {
     @Inject
     private AttachService attachService;
 
-    @RequestParameters("fileId")
+    @GetMapping("/delete-file/{fileId}")
     public void deleteAttach(String fileId) throws BusinessException {
         this.attachService.deleteAttach(fileId);
     }
